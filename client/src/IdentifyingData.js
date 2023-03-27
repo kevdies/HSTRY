02af7ddff341b2a84c44d17f71c4c961023b48dd
Kevin Diesenberg <kdiesenb@gmail.com>
2023-03-26 21:29:56 -0400

    identifying data component.  also created a collpase for each section

--- client/src/IdentifyingData.js
+++ client/src/IdentifyingData.js
@@ -1,17 +1,234 @@
-import React from "react";
-import { FormGroup, Label, Input } from "reactstrap";
+import React, { useState } from "react";
+import { FormGroup, Label, Input, Container, Row, Col, Button, Collapse } from "reactstrap";
 
 const IdentifyingData = () => {
+  const [appHandedness, setAppHandedness] = useState("");
+  const [appAge, setAppAge] = useState("");
+  const [appHeightFeet, setAppHeightFeet] = useState("");
+  const [appHeightInches, setAppHeightInches] = useState("");
+  const [appWeight, setAppWeight] = useState("");
+  const [appAddress1, setAppAddress1] = useState("");
+  const [appAddress2, setAppAddress2] = useState("");
+  const [appCity, setAppCity] = useState("");
+  const [appState, setAppState] = useState("");
+  const [appZip, setAppZip] = useState("");
+  const [appDOB, setAppDOB] = useState("");
+  const [appBodyPart, setAppBodyPart] = useState("");
+  const [appDOI, setAppDOI] = useState("");
+  const [appEmployeeTitle, setAppEmployeeTitle] = useState("");
+  const [appEmployer, setAppEmployer] = useState("");
+
+    //toggle collapse of form group
+    const [isOpen, setIsOpen] = useState(false);
+    const toggle = () => setIsOpen(!isOpen);
+
   return (
-    <FormGroup>
-      <Label for="identifyingData">Identifying Data</Label>
-      <Input
-        type="text"
-        name="identifyingData"
-        id="identifyingData"
-        placeholder="Enter identifying data"
-      />
-    </FormGroup>
+    <>
+      <Container className="container-form">
+        <Row>
+          <Col>
+            <h3>Identifying Data</h3>
+            <Button onClick={toggle} style={{ marginBottom: "1rem" }}>
+              {isOpen ? "Close" : "Open"}
+            </Button>
+            <Collapse isOpen={isOpen}>
+              <FormGroup className="custom-form-group">
+                <Label for="appHandedness">Applicant Handedness</Label>
+                <Input
+                  className="form-control mb-3"
+                  type="text"
+                  name="appHandedness"
+                  id="appHandedness"
+                  placeholder="Enter applicant handedness"
+                  value={appHandedness}
+                  onChange={(e) => setAppHandedness(e.target.value)}
+                />
+              </FormGroup>
+
+              <FormGroup className="custom-form-group">
+                <Label for="appAge">Applicant Age</Label>
+                <Input
+                  className="form-control mb-3"
+                  type="number"
+                  name="appAge"
+                  id="appAge"
+                  placeholder="Enter applicant age"
+                  value={appAge}
+                  onChange={(e) => setAppAge(e.target.value)}
+                />
+              </FormGroup>
+
+              <FormGroup className="custom-form-group">
+                <Label for="appHeightFeet">Applicant Height (Feet)</Label>
+                <Input
+                  className="form-control mb-3"
+                  type="number"
+                  name="appHeightFeet"
+                  id="appHeightFeet"
+                  placeholder="Enter applicant height in feet"
+                  value={appHeightFeet}
+                  onChange={(e) => setAppHeightFeet(e.target.value)}
+                />
+              </FormGroup>
+
+              <FormGroup className="custom-form-group">
+                <Label for="appHeightInches">Applicant Height (Inches)</Label>
+                <Input
+                  className="form-control mb-3"
+                  type="number"
+                  name="appHeightInches"
+                  id="appHeightInches"
+                  placeholder="Enter applicant height in inches"
+                  value={appHeightInches}
+                  onChange={(e) => setAppHeightInches(e.target.value)}
+                />
+              </FormGroup>
+              <FormGroup className="custom-form-group">
+                <Label for="appWeight">Applicant Weight</Label>
+                <Input
+                  className="form-control mb-3"
+                  type="number"
+                  name="appWeight"
+                  id="appWeight"
+                  placeholder="Enter applicant weight"
+                  value={appWeight}
+                  onChange={(e) => setAppWeight(e.target.value)}
+                />
+              </FormGroup>
+
+              <FormGroup className="custom-form-group">
+                <Label for="appAddress1">Applicant Address 1</Label>
+                <Input
+                  className="form-control mb-3"
+                  type="text"
+                  name="appAddress1"
+                  id="appAddress1"
+                  placeholder="Enter applicant address 1"
+                  value={appAddress1}
+                  onChange={(e) => setAppAddress1(e.target.value)}
+                />
+              </FormGroup>
+
+              <FormGroup className="custom-form-group">
+                <Label for="appAddress2">Applicant Address 2</Label>
+                <Input
+                  className="form-control mb-3"
+                  type="text"
+                  name="appAddress2"
+                  id="appAddress2"
+                  placeholder="Enter applicant address 2"
+                  value={appAddress2}
+                  onChange={(e) => setAppAddress2(e.target.value)}
+                />
+              </FormGroup>
+
+              <FormGroup className="custom-form-group">
+                <Label for="appCity">Applicant City</Label>
+                <Input
+                  className="form-control mb-3"
+                  type="text"
+                  name="appCity"
+                  id="appCity"
+                  placeholder="Enter applicant city"
+                  value={appCity}
+                  onChange={(e) => setAppCity(e.target.value)}
+                />
+              </FormGroup>
+
+              <FormGroup className="custom-form-group">
+                <Label for="appState">Applicant State</Label>
+                <Input
+                  className="form-control mb-3"
+                  type="text"
+                  name="appState"
+                  id="appState"
+                  placeholder="Enter applicant state"
+                  value={appState}
+                  onChange={(e) => setAppState(e.target.value)}
+                />
+              </FormGroup>
+
+              <FormGroup className="custom-form-group">
+                <Label for="appZip">Applicant Zip</Label>
+                <Input
+                  className="form-control mb-3"
+                  type="text"
+                  name="appZip"
+                  id="appZip"
+                  placeholder="Enter applicant zip"
+                  value={appZip}
+                  onChange={(e) => setAppZip(e.target.value)}
+                />
+              </FormGroup>
+
+              <FormGroup className="custom-form-group">
+                <Label for="appDOB">Applicant Date of Birth</Label>
+                <Input
+                  className="form-control mb-3"
+                  type="date"
+                  name="appDOB"
+                  id="appDOB"
+                  placeholder="Enter applicant date of birth"
+                  value={appDOB}
+                  onChange={(e) => setAppDOB(e.target.value)}
+                />
+              </FormGroup>
+
+              <FormGroup className="custom-form-group">
+                <Label for="appBodyPart">Applicant Body Part</Label>
+                <Input
+                  className="form-control mb-3"
+                  type="text"
+                  name="appBodyPart"
+                  id="appBodyPart"
+                  placeholder="Enter applicant body part"
+                  value={appBodyPart}
+                  onChange={(e) => setAppBodyPart(e.target.value)}
+                />
+              </FormGroup>
+              <FormGroup className="custom-form-group">
+                <Label for="appDOI">Applicant Date of Injury</Label>
+                <Input
+                  className="form-control mb-3"
+                  type="date"
+                  name="appDOI"
+                  id="appDOI"
+                  placeholder="Enter applicant date of injury"
+                  value={appDOI}
+                  onChange={(e) => setAppDOI(e.target.value)}
+                />
+              </FormGroup>
+
+              <FormGroup className="custom-form-group">
+                <Label for="appEmployeeTitle">Applicant Employee Title</Label>
+                <Input
+                  className="form-control mb-3"
+                  type="text"
+                  name="appEmployeeTitle"
+                  id="appEmployeeTitle"
+                  placeholder="Enter applicant employee title"
+                  value={appEmployeeTitle}
+                  onChange={(e) => setAppEmployeeTitle(e.target.value)}
+                />
+              </FormGroup>
+
+              <FormGroup className="custom-form-group">
+                <Label for="appEmployer">Applicant Employer</Label>
+                <Input
+                  className="form-control mb-3"
+                  type="text"
+                  name="appEmployer"
+                  id="appEmployer"
+                  placeholder="Enter applicant employer"
+                  value={appEmployer}
+                  onChange={(e) => setAppEmployer(e.target.value)}
+                />
+              </FormGroup>
+            </Collapse>
+          </Col>
+        </Row>
+      </Container>
+    </>
   );
 };
 
--- client/src/Intro.js
+++ client/src/Intro.js
@@ -1,5 +1,14 @@
 import React, { useState } from "react";
-import { FormGroup, Label, Input, Container, Row, Col } from "reactstrap";
+import {
+  FormGroup,
+  Label,
+  Input,
+  Container,
+  Row,
+  Col,
+  Button,
+  Collapse,
+} from "reactstrap";
 
 const Intro = () => {
   const [appName, setAppName] = useState("");
@@ -7,63 +16,72 @@ const Intro = () => {
   const [appDOI, setAppDOI] = useState("");
   const [historyTranscriber, setHistoryTranscriber] = useState("");
 
+  //toggle collapse of form group
+  const [isOpen, setIsOpen] = useState(false);
+  const toggle = () => setIsOpen(!isOpen);
+
   return (
     <>
       <Container className="container-form">
         <Row>
           <Col>
             <h3>Introduction</h3>
-            <FormGroup className="custom-form-group">
-              <Label for="appName">Applicant Name</Label>
-              <Input
-                className="form-control mb-3"
-                type="text"
-                name="appName"
-                id="appName"
-                placeholder="Enter applicant name"
-                value={appName}
-                onChange={(e) => setAppName(e.target.value)}
-              />
-            </FormGroup>
+            <Button onClick={toggle} style={{ marginBottom: "1rem" }}>
+              {isOpen ? "Close" : "Open"}
+            </Button>
+            <Collapse isOpen={isOpen}>
+              <FormGroup className="custom-form-group">
+                <Label for="appName">Applicant Name</Label>
+                <Input
+                  className="form-control mb-3"
+                  type="text"
+                  name="appName"
+                  id="appName"
+                  placeholder="Enter applicant name"
+                  value={appName}
+                  onChange={(e) => setAppName(e.target.value)}
+                />
+              </FormGroup>
 
-            <FormGroup className="custom-form-group">
-              <Label for="appBodyPart">Applicant Body Part</Label>
-              <Input
-                className="form-control mb-3"
-                type="text"
-                name="appBodyPart"
-                id="appBodyPart"
-                placeholder="Enter applicant body part"
-                value={appBodyPart}
-                onChange={(e) => setAppBodyPart(e.target.value)}
-              />
-            </FormGroup>
+              <FormGroup className="custom-form-group">
+                <Label for="appBodyPart">Applicant Body Part</Label>
+                <Input
+                  className="form-control mb-3"
+                  type="text"
+                  name="appBodyPart"
+                  id="appBodyPart"
+                  placeholder="Enter applicant body part"
+                  value={appBodyPart}
+                  onChange={(e) => setAppBodyPart(e.target.value)}
+                />
+              </FormGroup>
 
-            <FormGroup className="custom-form-group">
-              <Label for="appDOI">Applicant Date of Injury</Label>
-              <Input
-                className="form-control mb-3"
-                type="date"
-                name="appDOI"
-                id="appDOI"
-                placeholder="Enter applicant date of injury"
-                value={appDOI}
-                onChange={(e) => setAppDOI(e.target.value)}
-              />
-            </FormGroup>
+              <FormGroup className="custom-form-group">
+                <Label for="appDOI">Applicant Date of Injury</Label>
+                <Input
+                  className="form-control mb-3"
+                  type="date"
+                  name="appDOI"
+                  id="appDOI"
+                  placeholder="Enter applicant date of injury"
+                  value={appDOI}
+                  onChange={(e) => setAppDOI(e.target.value)}
+                />
+              </FormGroup>
 
-            <FormGroup className="custom-form-group">
-              <Label for="historyTranscriber">History Transcriber</Label>
-              <Input
-                className="form-control mb-3"
-                type="text"
-                name="historyTranscriber"
-                id="historyTranscriber"
-                placeholder="Enter history transcriber"
-                value={historyTranscriber}
-                onChange={(e) => setHistoryTranscriber(e.target.value)}
-              />
-            </FormGroup>
+              <FormGroup className="custom-form-group">
+                <Label for="historyTranscriber">History Transcriber</Label>
+                <Input
+                  className="form-control mb-3"
+                  type="text"
+                  name="historyTranscriber"
+                  id="historyTranscriber"
+                  placeholder="Enter history transcriber"
+                  value={historyTranscriber}
+                  onChange={(e) => setHistoryTranscriber(e.target.value)}
+                />
+              </FormGroup>
+            </Collapse>
           </Col>
         </Row>
       </Container>
