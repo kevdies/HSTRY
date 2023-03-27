60d8fe26a2fb364ce2c3d7af50cef32d6d61dd99
Kevin Diesenberg <kdiesenb@gmail.com>
2023-03-27 12:33:36 -0400

    round one on inputs for job description. updated css styles for textarea input and focus

--- client/src/JobDescription.js
+++ client/src/JobDescription.js
@@ -10,9 +10,18 @@ import {
 } from "reactstrap";
 
 const JobDescription = () => {
+  const [appJobTitle, setAppJobTitle] = useState("");
+  const [appCompany, setAppCompany] = useState("");
+  const [appEmploymentDateBegin, setAppEmploymentDateBegin] = useState("");
+  const [appEmploymentDateEnd, setAppEmploymentDateEnd] = useState("");
+  const [doesApplicantWork, setDoesApplicantWork] = useState(false);
+  const [workHoursPerDay, setWorkHoursPerDay] = useState("");
+  const [workHoursPerWeek, setWorkHoursPerWeek] = useState("");
   const [jobDescription, setJobDescription] = useState("");
+  const [toolsAndBodyParts, setToolsAndBodyParts] = useState("");
+  const [appExposure, setAppExposure] = useState("");
+  const [appPhysicalJobActivities, setAppPhysicalJobActivities] = useState("");
 
-  //toggle collapse of form group
   const [isOpen, setIsOpen] = useState(false);
   const toggle = () => setIsOpen(!isOpen);
 
@@ -40,15 +49,170 @@ const JobDescription = () => {
 
             <Collapse isOpen={isOpen}>
               <FormGroup className="custom-form-group">
-                <Label for="jobDescription">Job Description</Label>
+                <Label for="appJobTitle">Applicant Job Title</Label>
                 <Input
                   className="form-control mb-3"
                   type="text"
+                  name="appJobTitle"
+                  id="appJobTitle"
+                  placeholder="Enter applicant job title"
+                  value={appJobTitle}
+                  onChange={(e) => setAppJobTitle(e.target.value)}
+                />
+              </FormGroup>
+
+              <FormGroup className="custom-form-group">
+                <Label for="appCompany">Applicant Company</Label>
+                <Input
+                  className="form-control mb-3"
+                  type="text"
+                  name="appCompany"
+                  id="appCompany"
+                  placeholder="Enter applicant company"
+                  value={appCompany}
+                  onChange={(e) => setAppCompany(e.target.value)}
+                />
+              </FormGroup>
+
+              <FormGroup className="custom-form-group">
+                <Label for="appEmploymentDateBegin">
+                  Applicant Employment Date Begin
+                </Label>
+                <Input
+                  className="form-control mb-3"
+                  type="date"
+                  name="appEmploymentDateBegin"
+                  id="appEmploymentDateBegin"
+                  placeholder="Enter applicant employment date begin"
+                  value={appEmploymentDateBegin}
+                  onChange={(e) => setAppEmploymentDateBegin(e.target.value)}
+                />
+              </FormGroup>
+
+              <FormGroup className="custom-form-group">
+                <Label for="appEmploymentDateEnd">
+                  Applicant Employment Date End
+                </Label>
+                <Input
+                  className="form-control mb-3"
+                  type="date"
+                  name="appEmploymentDateEnd"
+                  id="appEmploymentDateEnd"
+                  placeholder="Enter applicant employment date end"
+                  value={appEmploymentDateEnd}
+                  onChange={(e) => setAppEmploymentDateEnd(e.target.value)}
+                />
+              </FormGroup>
+
+              <FormGroup className="custom-form-group">
+                <Label for="doesApplicantWork">
+                  Does the applicant still work?
+                </Label>
+                <FormGroup check>
+                  <Label check>
+                    <Input
+                      type="radio"
+                      name="doesApplicantWork"
+                      value="true"
+                      checked={doesApplicantWork}
+                      onChange={(e) => setDoesApplicantWork(true)}
+                    />{" "}
+                    Yes
+                  </Label>
+                </FormGroup>
+                <FormGroup check>
+                  <Label check>
+                    <Input
+                      type="radio"
+                      name="doesApplicantWork"
+                      value="false"
+                      checked={!doesApplicantWork}
+                      onChange={(e) => setDoesApplicantWork(false)}
+                    />{" "}
+                    No
+                  </Label>
+                </FormGroup>
+              </FormGroup>
+
+              <FormGroup className="custom-form-group">
+                <Label for="workHoursPerDay">Work Hours Per Day</Label>
+                <Input
+                  className="form-control mb-3"
+                  type="number"
+                  name="workHoursPerDay"
+                  id="workHoursPerDay"
+                  placeholder="Enter work hours per day"
+                  value={workHoursPerDay}
+                  onChange={(e) => setWorkHoursPerDay(e.target.value)}
+                />
+              </FormGroup>
+
+              <FormGroup className="custom-form-group">
+                <Label for="workHoursPerWeek">Work Hours Per Week</Label>
+                <Input
+                  className="form-control mb-3"
+                  type="number"
+                  name="workHoursPerWeek"
+                  id="workHoursPerWeek"
+                  placeholder="Enter work hours per week"
+                  value={workHoursPerWeek}
+                  onChange={(e) => setWorkHoursPerWeek(e.target.value)}
+                />
+              </FormGroup>
+
+              <FormGroup className="custom-form-group">
+                <Label for="jobDescription">Job Description</Label>
+                <Input
+                  className="form-control mb-3"
+                  type="textarea"
                   name="jobDescription"
                   id="jobDescription"
                   placeholder="Enter job description"
                   value={jobDescription}
                   onChange={(e) => setJobDescription(e.target.value)}
+                />
+              </FormGroup>
+
+              <FormGroup className="custom-form-group">
+                <Label for="toolsAndBodyParts">
+                  Tools and Body Parts to Perform Job
+                </Label>
+                <Input
+                  className="form-control mb-3"
+                  type="textarea"
+                  name="toolsAndBodyParts"
+                  id="toolsAndBodyParts"
+                  placeholder="Enter tools and body parts to perform job"
+                  value={toolsAndBodyParts}
+                  onChange={(e) => setToolsAndBodyParts(e.target.value)}
+                />
+              </FormGroup>
+
+              <FormGroup className="custom-form-group">
+                <Label for="appExposure">Applicant Exposure</Label>
+                <Input
+                  className="form-control mb-3"
+                  type="textarea"
+                  name="appExposure"
+                  id="appExposure"
+                  placeholder="Enter applicant exposure"
+                  value={appExposure}
+                  onChange={(e) => setAppExposure(e.target.value)}
+                />
+              </FormGroup>
+
+              <FormGroup className="custom-form-group">
+                <Label for="appPhysicalJobActivities">
+                  Applicant Physical Job Activities
+                </Label>
+                <Input
+                  className="form-control mb-3"
+                  type="textarea"
+                  name="appPhysicalJobActivities"
+                  id="appPhysicalJobActivities"
+                  placeholder="Enter applicant physical job activities"
+                  value={appPhysicalJobActivities}
+                  onChange={(e) => setAppPhysicalJobActivities(e.target.value)}
                 />
               </FormGroup>
             </Collapse>
