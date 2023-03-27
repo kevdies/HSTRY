3b343ea474e276c6b6970ccffbf90e6eec359abb
Kevin Diesenberg <kdiesenb@gmail.com>
2023-03-27 00:14:39 -0400

    social history inputs and types updated

--- client/src/SocialHistory.js
+++ client/src/SocialHistory.js
@@ -10,7 +10,16 @@ import {
 } from "reactstrap";
 
 const SocialHistory = () => {
-  const [socialHistory, setSocialHistory] = useState("");
+  // const [socialHistory, setSocialHistory] = useState("");
+  const [maritalStatus, setMaritalStatus] = useState("");
+  const [hasChildren, setHasChildren] = useState(false);
+  const [alcoholUse, setAlcoholUse] = useState(false);
+  const [tobaccoUse, setTobaccoUse] = useState(false);
+  const [marijuanaUse, setMarijuanaUse] = useState(false);
+  const [hasOtherDrugUse, setHasOtherDrugUse] = useState("");
+  const [otherDrugList, setOtherDrugList] = useState("");
+  const [numChildren, setNumChildren] = useState("")
+
 
   const [isOpen, setIsOpen] = useState(false);
   const toggle = () => setIsOpen(!isOpen);
@@ -21,7 +30,7 @@ const SocialHistory = () => {
         <Row>
           <Col>
             <h3>
-             Social History{" "}
+              Social History{" "}
               {isOpen ? (
                 <i
                   className="bi bi-caret-up-fill"
@@ -39,16 +48,156 @@ const SocialHistory = () => {
 
             <Collapse isOpen={isOpen}>
               <FormGroup className="custom-form-group">
-                <Label for="socialHistory">Social History</Label>
-                <Input
-                  className="form-control mb-3"
-                  type="text"
-                  name="socialHistory"
-                  id="socialHistory"
-                  placeholder="Enter social history"
-                  value={socialHistory}
-                  onChange={(e) => setSocialHistory(e.target.value)}
-                />
+                <Label>Marital Status</Label>
+                <FormGroup check>
+                  <Label check>
+                    <Input
+                      type="radio"
+                      name="maritalStatus"
+                      id="single"
+                      checked={maritalStatus === "single"}
+                      onChange={(e) => setMaritalStatus("single")}
+                    />{" "}
+                    Single
+                  </Label>
+                </FormGroup>
+                <FormGroup check>
+                  <Label check>
+                    <Input
+                      type="radio"
+                      name="maritalStatus"
+                      id="married"
+                      checked={maritalStatus === "married"}
+                      onChange={(e) => setMaritalStatus("married")}
+                    />{" "}
+                    Married
+                  </Label>
+                </FormGroup>
+                <FormGroup check>
+                  <Label check>
+                    <Input
+                      type="radio"
+                      name="maritalStatus"
+                      id="divorced"
+                      checked={maritalStatus === "divorced"}
+                      onChange={(e) => setMaritalStatus("divorced")}
+                    />{" "}
+                    Divorced
+                  </Label>
+                </FormGroup>
+                <FormGroup check>
+                  <Label check>
+                    <Input
+                      type="radio"
+                      name="maritalStatus"
+                      id="separated"
+                      checked={maritalStatus === "separated"}
+                      onChange={(e) => setMaritalStatus("separated")}
+                    />{" "}
+                    Separated
+                  </Label>
+                </FormGroup>
+              </FormGroup>
+
+              <FormGroup className="custom-form-group">
+                <Label>Children</Label>
+                <FormGroup check>
+                  <Label check>
+                    <Input
+                      type="checkbox"
+                      name="hasChildren"
+                      id="hasChildren"
+                      checked={hasChildren}
+                      onChange={(e) => setHasChildren(e.target.checked)}
+                    />{" "}
+                    Yes
+                  </Label>
+                </FormGroup>
+                {hasChildren && (
+                  <Input
+                    className="form-control mb-3"
+                    type="number"
+                    name="numChildren"
+                    id="numChildren"
+                    placeholder="Enter number of children"
+                    value={numChildren}
+                    onChange={(e) => setNumChildren(e.target.value)}
+                  />
+                )}
+              </FormGroup>
+
+              <FormGroup className="custom-form-group">
+                <Label>Alcohol Use</Label>
+                <FormGroup check>
+                  <Label check>
+                    <Input
+                      type="checkbox"
+                      name="alcoholUse"
+                      id="alcoholUse"
+                      checked={alcoholUse}
+                      onChange={(e) => setAlcoholUse(e.target.checked)}
+                    />{" "}
+                    Yes
+                  </Label>
+                </FormGroup>
+              </FormGroup>
+
+              <FormGroup className="custom-form-group">
+                <Label>Tobacco Use</Label>
+                <FormGroup check>
+                  <Label check>
+                    <Input
+                      type="checkbox"
+                      name="tobaccoUse"
+                      id="tobaccoUse"
+                      checked={tobaccoUse}
+                      onChange={(e) => setTobaccoUse(e.target.checked)}
+                    />{" "}
+                    Yes
+                  </Label>
+                </FormGroup>
+              </FormGroup>
+
+              <FormGroup className="custom-form-group">
+                <Label>Marijuana Use</Label>
+                <FormGroup check>
+                  <Label check>
+                    <Input
+                      type="checkbox"
+                      name="marijuanaUse"
+                      id="marijuanaUse"
+                      checked={marijuanaUse}
+                      onChange={(e) => setMarijuanaUse(e.target.checked)}
+                    />{" "}
+                    Yes
+                  </Label>
+                </FormGroup>
+              </FormGroup>
+              <FormGroup className="custom-form-group">
+                <Label>Other Drug Use</Label>
+                <FormGroup check>
+                  <Label check>
+                    <Input
+                      type="checkbox"
+                      name="hasOtherDrugUse"
+                      id="hasOtherDrugUse"
+                      checked={hasOtherDrugUse}
+                      onChange={(e) => setHasOtherDrugUse(e.target.checked)}
+                    />{" "}
+                    Yes
+                  </Label>
+                </FormGroup>
+                {hasOtherDrugUse && (
+                  <Input
+                    className="form-control mb-3"
+                    type="text"
+                    name="otherDrugList"
+                    id="otherDrugList"
+                    placeholder="Enter other drugs used"
+                    value={otherDrugList}
+                    onChange={(e) => setOtherDrugList(e.target.value)}
+                  />
+                )}
               </FormGroup>
             </Collapse>
           </Col>
