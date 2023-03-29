14f2999f0a97508d7b903d2e676d5ce58b100ef5
Kevin Diesenberg <kdiesenb@gmail.com>
2023-03-29 16:05:43 -0400

    current work status updated with inputs

--- client/src/CurrentWorkStatus.js
+++ client/src/CurrentWorkStatus.js
@@ -11,6 +11,9 @@ import {
 
 const CurrentWorkStatus = () => {
   const [currentWorkStatus, setCurrentWorkStatus] = useState("");
+  const [stillEmployedBy, setStillEmployedBy] = useState("");
+  const [currentDisabilityBenefits, setCurrentDisabilityBenefits] =
+    useState("");
 
   const [isOpen, setIsOpen] = useState(false);
   const toggle = () => setIsOpen(!isOpen);
@@ -42,12 +45,38 @@ const CurrentWorkStatus = () => {
                 <Label for="currentWorkStatus">Current Work Status</Label>
                 <Input
                   className="form-control mb-3"
-                  type="text"
+                  type="textarea"
                   name="currentWorkStatus"
                   id="currentWorkStatus"
-                  placeholder="Enter current work status"
+                  placeholder="Enter current work status and with what company"
                   value={currentWorkStatus}
                   onChange={(e) => setCurrentWorkStatus(e.target.value)}
+                />
+              </FormGroup>
+              <FormGroup className="custom-form-group">
+                <Label for="stillEmployedBy">Still Employed By</Label>
+                <Input
+                  className="form-control mb-3"
+                  type="textarea"
+                  name="stillEmployedBy"
+                  id="stillEmployedBy"
+                  placeholder="Enter if the applicant is still employed and for who"
+                  value={stillEmployedBy}
+                  onChange={(e) => setStillEmployedBy(e.target.value)}
+                />
+              </FormGroup>
+              <FormGroup className="custom-form-group">
+                <Label for="currentDisabilityBenefits">
+                  Current Disability Benefits
+                </Label>
+                <Input
+                  className="form-control mb-3"
+                  type="textarea"
+                  name="currentDisabilityBenefits"
+                  id="currentDisabilityBenefits"
+                  placeholder="Enter any current disability benefits"
+                  value={currentDisabilityBenefits}
+                  onChange={(e) => setCurrentDisabilityBenefits(e.target.value)}
                 />
               </FormGroup>
             </Collapse>
