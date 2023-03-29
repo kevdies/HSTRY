8de35dc346c7e3e97bfec6eb2e6cd182bb22cd38
Kevin Diesenberg <kdiesenb@gmail.com>
2023-03-29 16:29:16 -0400

    other problems updated inputs

--- client/src/OtherProblems.js
+++ client/src/OtherProblems.js
@@ -10,23 +10,9 @@ import {
 } from "reactstrap";
 
 const OtherProblems = () => {
-  const [hasOtherProblems, setHasOtherProblems] = useState(false);
- const [stress, setStress] = useState(false);
- const [anxiety, setAnxiety] = useState(false);
- const [depression, setDepression] = useState(false);
- const [insomnia, setInsomnia] = useState(false);
-  const [stomachIssues, setStomachIssues] = useState(false);
   const [otherProblems, setOtherProblems] = useState("");
-
-
-
   const [isOpen, setIsOpen] = useState(false);
   const toggle = () => setIsOpen(!isOpen);
-
-  const handleOtherProblemsChange = (e) => {
-    const { value } = e.target;
-    setHasOtherProblems(value === "yes");
-  };
 
   return (
     <>
@@ -52,95 +38,17 @@ const OtherProblems = () => {
 
             <Collapse isOpen={isOpen}>
               <FormGroup className="custom-form-group">
-                <Label>Does the applicant have other problems?</Label>
-                <FormGroup check>
-                  <Label check>
-                    <Input
-                      type="radio"
-                      name="hasOtherProblems"
-                      value="no"
-                      checked={!hasOtherProblems}
-                      onChange={handleOtherProblemsChange}
-                    />{" "}
-                    No
-                  </Label>
-                </FormGroup>
-                <FormGroup check>
-                  <Label check>
-                    <Input
-                      type="radio"
-                      name="hasOtherProblems"
-                      value="yes"
-                      checked={hasOtherProblems}
-                      onChange={handleOtherProblemsChange}
-                    />{" "}
-                    Yes
-                  </Label>
-                </FormGroup>
-              </FormGroup>
-
-              {hasOtherProblems && (
-                <>
-                  <FormGroup>
-                    <Label>Check all that apply:</Label>
-                    <FormGroup check>
-                      <Label check>
-                        <Input
-                          type="checkbox"
-                          onChange={(e) => setStress(e.target.checked)}
-                        />{" "}
-                        Stress
-                      </Label>
-                    </FormGroup>
-                    <FormGroup check>
-                      <Label check>
-                        <Input
-                          type="checkbox"
-                          onChange={(e) => setAnxiety(e.target.checked)}
-                        />{" "}
-                        Anxiety
-                      </Label>
-                    </FormGroup>
-                    <FormGroup check>
-                      <Label check>
-                        <Input
-                          type="checkbox"
-                          onChange={(e) => setDepression(e.target.checked)}
-                        />{" "}
-                        Depression
-                      </Label>
-                    </FormGroup>
-                    <FormGroup check>
-                      <Label check>
-                        <Input
-                          type="checkbox"
-                          onChange={(e) => setInsomnia(e.target.checked)}
-                        />{" "}
-                        Insomnia
-                      </Label>
-                    </FormGroup>
-                    <FormGroup check>
-                      <Label check>
-                        <Input
-                          type="checkbox"
-                          onChange={(e) => setStomachIssues(e.target.checked)}
-                        />{" "}
-                        Stomach Issues
-                      </Label>
-                    </FormGroup>
-                    <FormGroup>
+                <FormGroup>
                       <Label for="otherProblems">Other Problems:</Label>
                       <Input
                         type="textarea"
                         name="otherProblems"
                         id="otherProblems"
-                        placeholder="comma separated list of problems"
+                        placeholder="comma separated list of problems i.e. stress, anxiety, depression, insomnia, stomach issues, etc"
                         onChange={(e) => setOtherProblems(e.target.value)}
                       />
                     </FormGroup>
                   </FormGroup>
-                </>
-              )}
             </Collapse>
           </Col>
         </Row>
